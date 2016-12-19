6fb817e9c3894036b5666d216f4f8170f9ca105c
Andrew Chart <andrewc@andertons.co.uk>
2016-12-19 00:51:52 +0000

    Post text styling

--- wp-content/themes/andrewchart2017/post-default.php
+++ wp-content/themes/andrewchart2017/post-default.php
@@ -2,4 +2,11 @@
 <section class="main-content post-content">
   <h1><?php the_title(); ?></h1>
   <?php pBr(the_content()); ?>
+
+  <?php if(!empty(types_render_field('listening-to'))): ?>
+  <footer class="listening-to">
+    <div><?php echo types_render_field('listening-to'); ?></div>
+    <blockquote><?php echo types_render_field('listening-to-quote'); ?></blockquote>
+  </footer>
+  <?php endif; ?>
 </section>
--- wp-content/themes/andrewchart2017/post-with-hero.php
+++ wp-content/themes/andrewchart2017/post-with-hero.php
@@ -1,18 +1,18 @@
 <header class="post-hero-header">
+
   <div class="vignette"></div>
-
   <section class="post-title-area">
-
     <div class="title-column">
       <h1><?php the_title(); ?></h1>
       <section class="excerpt"><?php the_excerpt(); ?></section>
     </div>
 
     <div class="meta-column">
-
+      Published on <?php the_date(); ?> in <?php the_category(); ?>
     </div>
 
   </section>
+  <div class="see-more"><button class="material-icons scroll-down">expand_more</button></div>
 </header>
 
 <progress class="post-progress" value="0" max="100"></progress>
@@ -20,4 +20,12 @@
 <section class="main-content post-content">
 
   <?php pBr(the_content()); ?>
+
+  <?php if(!empty(types_render_field('listening-to'))): ?>
+  <footer class="listening-to">
+    <div><?php echo types_render_field('listening-to'); ?></div>
+    <blockquote><?php echo types_render_field('listening-to-quote'); ?></blockquote>
+  </footer>
+  <?php endif; ?>
+
 </section>
