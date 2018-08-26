75455457996bcbd9fc37025e48e909d55e4cfb11
Andrew Chart <andrew@andrewchart.co.uk>
2018-08-27 00:03:09 +0100

    Minor tweaks to Andertons category page image and homepage styling.

--- wp-content/themes/andrewchart2017/category-andertons.php
+++ wp-content/themes/andrewchart2017/category-andertons.php
@@ -9,12 +9,12 @@
 <header class="page-hero-header">
 
   <div class="hero-image-container"
-    data-hero-image-xl="<?php echo wp_get_attachment_image_src(238, 'uncropped_xl')[0]; ?>"
-    data-hero-image-l="<?php echo wp_get_attachment_image_src(238, 'uncropped_l')[0]; ?>"
-    data-hero-image-m="<?php echo wp_get_attachment_image_src(238, 'uncropped_m')[0]; ?>"
-    data-hero-image-s="<?php echo wp_get_attachment_image_src(238, 'uncropped_s')[0]; ?>">
+    data-hero-image-xl="<?php echo wp_get_attachment_image_src(413, 'uncropped_xl')[0]; ?>"
+    data-hero-image-l="<?php echo wp_get_attachment_image_src(413, 'uncropped_l')[0]; ?>"
+    data-hero-image-m="<?php echo wp_get_attachment_image_src(413, 'uncropped_m')[0]; ?>"
+    data-hero-image-s="<?php echo wp_get_attachment_image_src(413, 'uncropped_s')[0]; ?>">
     <div class="hero-fullres-container"></div>
-    <div class="hero-placeholder-container" style="background-image: url('<?php echo wp_get_attachment_image_src(238, 'uncropped_tiny')[0]; ?>')"></div>
+    <div class="hero-placeholder-container" style="background-image: url('<?php echo wp_get_attachment_image_src(413, 'uncropped_tiny')[0]; ?>')"></div>
   </div>
 
   <div class="vignette"></div>
@@ -22,9 +22,9 @@
   <section class="post-title-area">
     <div class="title-column">
       <h1><?php single_cat_title(); ?></h1>
-      <section class="excerpt"><p>As a longstanding member of the digital team,
-        I have supported the growth of this family musical instrument retailer into a
-        multi-million pound, international brand.</p></section>
+      <section class="excerpt"><p>As the head of the digital team, I have supported
+        the growth of this family musical instrument retailer into a multi-million
+        pound, international brand.</p></section>
     </div>
   </section>
 
