b0fad516fdea8d8d96edc9c0386c130753f07a8e
Andrew Chart <achart@hotmail.com>
2022-11-10 19:17:12 +0000

    Update Yoast to 17.7.1

--- wp-content/plugins/wordpress-seo/vendor/autoload.php
+++ wp-content/plugins/wordpress-seo/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInitb8fceefcda1c4d18916d08a8d7a7d3e0::getLoader();
+return ComposerAutoloaderInit1784eb03537fd1492f50df46ca25d00c::getLoader();
