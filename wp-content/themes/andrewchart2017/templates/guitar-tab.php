e722ad803e6127728e8c3e6f0f8d30d9374804c6
Andrew Chart <achart@hotmail.com>
2022-11-12 13:05:14 +0000

    Guitar tab responsiveness

--- wp-content/themes/andrewchart2017/templates/guitar-tab.php
+++ wp-content/themes/andrewchart2017/templates/guitar-tab.php
@@ -1,9 +1,6 @@
 <article class="guitar-tab">
     <?php if (strlen($tab_content) > 0) : ?>
-
-        <pre>
-            <?= $tab_content ?>    
-        </pre>
+        <pre><?= $tab_content ?></pre>
     <?php else: ?>
        <span class="error">Error, could not display guitar tab</span>';
     <?php endif ?>
